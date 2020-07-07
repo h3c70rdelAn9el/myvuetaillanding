3de9769faea16362553256d4e2c4d87ffefffa89
Hector del Angel <hectordelangel@gmail.com>
2020-07-07 14:22:02 -0700

    Add call to action section

---
+++ src/components/ActionCall.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="call h-200 -mt-32">
+    <div class="container mx-auto px-6 text-center py-20">
+      <h2 class="my-4 text-2xl text-gray-100">
+        Now in Stock
+      </h2>
+      <h3 class="my-4 text-2xl text-gray-100">
+        Get in touch with us
+      </h3>
+      <button class="bg-white font-bold rounded-full mt-6 py-4 px-8 shadow-lg uppercase tracking-wider">
+        Contact us
+      </button>
+    </div>
+    <!-- <div class="h-200 w-200" style="background-image: url(../assets/img/longboardgirl.jpg)">
+      <img src="../assets/img/longboardgirl.jpg" alt="">
+    </div> -->
+  </section>
+</template>
+
+<style>
+  .call {
+    background-image: url(../assets/img/longboardgirl.jpg);
+    background-size: cover;
+    opacity: .8;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <Hero />
     <Features />
+    <ActionCall />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import Hero from '@/components/Hero.vue';
 import Features from '@/components/Features.vue'
+import ActionCall from '@/components/ActionCall.vue'
 
 export default {
   name: 'Home',
   components: {
     Hero,
-    Features
+    Features,
+    ActionCall
   }
 }
 </script>
